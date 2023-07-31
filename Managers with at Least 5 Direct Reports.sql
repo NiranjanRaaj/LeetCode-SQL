1cb1d72255d3110b9e185c73fcb524b2d8160ce3
Niranjan Raaj <36319133+NiranjanRaaj@users.noreply.github.com>
2023-07-31 20:00:52 +0530

    Create Managers with at Least 5 Direct Reports.sql

---
+++ Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,64 @@
+/*
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int)
+Truncate table Employee
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', 'None')
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101')
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101')
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Find the managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | None      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+*/
+
+/*T-SQL query statement below */
+
+select e.name
+from employee e
+left join employee m
+on e.id = m.managerid
+group by e.id, e.name
+having count(m.managerID) >=5
